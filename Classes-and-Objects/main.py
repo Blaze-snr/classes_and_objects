d626a573340b051c8b04e5d736b8fa11bb9e6c6c
Blaze-snr <105287614+Blaze-snr@users.noreply.github.com>
2022-05-29 03:10:10 +0800

    Add files via upload

---
+++ Classes-and-Objects/main.py
@@ -0,0 +1,27 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        #Instance Variable
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+    def change_name(self):
+        print("Peter")
+
+    def change_age(self):
+        print("34")
+
+    def add_track(self):
+        print("UI/UX")
+
+    def get_score(self):
+        print("20.90")
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+Bob.change_name()
+Bob.change_age()
+Bob.add_track()
+Bob.get_score()
